38366ebc5170dda3b9518e5337e993009476a7b3
gobikris <96045276+gobikris@users.noreply.github.com>
2022-05-25 14:27:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ res.then((data)=>data.json()).catch((error)=> console.log(error))
         countryData.forEach(function(country) {
             let col = foo("div","col-lg-3 col-sm-12 col-md-6")
             col.innerHTML = `<div class=" z m-3 card border-success mb-3 shadow-lg  bg-dark " style="max-width: 18rem;">
-            <div class="card-header bg-secondary border-success text-center fw-bolder text-white ">${country.name.common}</div>
+            <div class="card-header bg-secondary border-success text-center fw-bolder text-white ">${country.name}</div>
             <div class="card-body text-success ">
               <img height="160" src="${country.flags.png}" class="card-img-top" alt="...">
             </div>
